d09acc24bc36b138d8537ad14bf2e3185b4104a5
Artem Rys <rysartem@gmail.com>
2024-11-11 16:55:07 +0100

    feat: delete checkpoint when input is removed (#6)
    This PR removes a relevant checkpoint from the KVStore when an input is
    deleted.
    
    Additionally it introduces a basic bash script to update local
    environment for the testing purposes.

--- package/bin/example_utils.py
+++ package/bin/example_utils.py
@@ -1,3 +1,6 @@
+ADDON_NAME = "Splunk_TA_Example"
+
+
 def get_example_collection_key_name(input_name: str) -> str:
     # `input_name` is a string like "example://<input_name>".
     return input_name.split("/")[-1]
