dc7ecb271062d94ce92902a0a6057a17bd5a61dd
hetangmodi-crest <hemodi@splunk.com>
2024-11-15 16:38:01 +0530

    ci: fixed docker script and updated requirements

--- scripts/run_locally.sh
+++ scripts/run_locally.sh
@@ -1,7 +1,11 @@
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+REPO_ROOT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
+
 cd ../
 docker compose down
 python3 -m venv .venv
 source .venv/bin/activate
+cd $REPO_ROOT_DIR
 pip install -r requirements-dev.txt
 ucc-gen build
 # running on ARM macOS
@@ -9,3 +13,4 @@ export DOCKER_DEFAULT_PLATFORM=linux/amd64
 docker compose up -d --build
 echo -n "Waiting Splunk for run"
 until curl -Lsk "https://localhost:8088/services/collector/health" &>/dev/null ; do echo -n "." && sleep 5 ; done
+ 
