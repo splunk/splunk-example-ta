e340f81cc5a42c76e5e987fa97889b9db463e7e5
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2024-11-26 14:23:54 +0100

    🐛

--- additional_packaging.py
+++ additional_packaging.py
@@ -8,7 +8,9 @@ def additional_packaging(addon_name: str) -> None:
     if path.exists(file_to_remove):
         os.remove(file_to_remove)
 
-    build_ui_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), "scripts", "build_ui.sh")
+    build_ui_script = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "scripts", "build_ui.sh"
+    )
     if path.exists(build_ui_script):
         os.system(f"chmod +x {build_ui_script}")
         return_code = os.system(build_ui_script)
