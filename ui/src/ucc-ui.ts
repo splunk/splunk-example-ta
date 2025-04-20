fb545e6aa4b04a3b01f14c1a5adf4ca2ee7f05e3
Szymon Oleksy <soleksy@splunk.com>
2025-04-20 14:59:04 +0200

    correct component structure

--- ui/src/ucc-ui.ts
+++ ui/src/ucc-ui.ts
@@ -3,8 +3,14 @@ import DateInputClass from "./ucc-ui-extensions/DateInput";
 import AdvancedInputsTabClass from "./ucc-ui-extensions/AdvancedInputsTab";
 
 init({
-  DateInput: DateInputClass,
-  AdvancedInputsTab: AdvancedInputsTabClass,
+  DateInput: {
+    component: DateInputClass,
+    type: 'control',
+  },
+  AdvancedInputsTab: {
+    component: AdvancedInputsTabClass,
+    type: 'tab',
+  },
 }).catch((error) => {
   console.error("Could not load UCC", error);
 });
